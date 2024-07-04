f29f3df2f157aad112aee38f37bd6a76b53c6c42
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-04 09:45:19 -0700

    Backend: Lógica para el manejo de las votaciones

--- models/band.js
+++ models/band.js
@@ -1,7 +1,11 @@
-class Band  {
-    constructor(name = 'no-name') {
-        //this.id = ???;
-        this.name = name;
-        this.votes = 0;
-    }
+const { v4: uuidV4 } = require("uuid");
+
+class Band {
+  constructor(name = "no-name") {
+    this.id = uuidV4();
+    this.name = name;
+    this.votes = 0;
+  }
 }
+
+module.exports = Band;
